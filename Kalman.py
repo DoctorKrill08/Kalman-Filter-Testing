fbd7018f24a4f7dcaae03018ecfa08bc66e69957
DoctorKrill08 <doctorkrill08@gmail.com>
2026-04-25 21:09:57 -0400

    okay real kalman filter seems to work this time

--- Kalman.py
+++ Kalman.py
@@ -5,23 +5,26 @@ import math
 import random
 
 class Limelight:
+    @staticmethod
+    def pose_to_sd(pose):
+        return pose / 5;
     def __init__(self):
         self.actual = 50
-        self.sd = 10
+        self.sd = self.pose_to_sd(self.actual)
         self.sdEstimate = self.sd
         self.reading = self.actual
 
     def setActual(self, actual):
         self.actual = actual
-        self.sd = actual / 5
+        self.sd = self.pose_to_sd(actual)
 
     def update(self):
         self.reading = self.sd * 2 * (0.5 - random.random()) + self.actual
-        self.sdEstimate = abs(self.reading) / 5
+        self.sdEstimate = self.pose_to_sd(self.reading)
 
 
 class KalmanFilter1D:
-    def __init__(self, initial_estimate=0, initial_uncertainty=1000):
+    def __init__(self, initial_estimate=1, initial_uncertainty=1000):
         self.x = initial_estimate
         self.P = initial_uncertainty
 
@@ -32,16 +35,21 @@ class KalmanFilter1D:
         S = self.P + R
 
         # --- TELEPORT / OUTLIER DETECTION ---
-        threshold = 3  # ~99.7% confidence (3-sigma rule)
+        threshold = 1.5  # ~99.7% confidence (3-sigma rule)
 
-        if abs(innovation) > threshold * math.sqrt(S):
+        sdError = abs(Limelight.pose_to_sd(self.x) - measurement_sd)
+        Q = (sdError / Limelight.pose_to_sd(self.x)) + abs(innovation)/(threshold * math.sqrt(abs(S)))
+
+        if abs(innovation) > threshold * math.sqrt(abs(S)) or (Q > threshold):
             # Option A: hard reset (fastest recovery)
             self.x = measurement
             self.P = R
             return self.x
 
+        self.P = self.P + Q
+
         # --- NORMAL KALMAN UPDATE ---
-        K = self.P / (self.P + R)
+        K = self.P  * 2/ (self.P + R)
         self.x = self.x + K * innovation
         self.P = (1 - K) * self.P
 
@@ -52,14 +60,18 @@ class KalmanFilter1D:
 limelight = Limelight()
 kf = KalmanFilter1D()
 
-for i in range(150):
+for i in range(250):
     limelight.update()
     estimate = kf.update(limelight.reading, limelight.sdEstimate)
     plt.scatter(i,limelight.reading,color = "red")
     plt.scatter(i,estimate,color = "blue")
     print(f"Measured: {limelight.reading:.2f}, Estimate: {estimate:.2f}, Actual: {limelight.actual}")
     if i == 50:
-        limelight.setActual(140)
+        limelight.setActual(60)
     if i == 100:
-        limelight.setActual(90)
+        limelight.setActual(140)
+    if i == 150:
+        limelight.setActual(120)
+    if i == 200:
+        limelight.setActual(20)
 plt.show()
