5b5785bd67e732dfd06c3c781397b4bd1428f67f
DoctorKrill08 <doctorkrill08@gmail.com>
2026-04-27 00:21:11 -0400

    Works well I think

--- Kalman.py
+++ Kalman.py
@@ -56,7 +56,7 @@ class KalmanFilter1D:
 
         z_score_of_error = abs(innovation) / measurement_sd
         # < 2 z-score -> nada. difference from 2
-        Q = (z_score_of_error ** 2) * (0.0003 * abs(innovation)) + (0.02 * abs(self.prev_x - self.x))
+        Q = (z_score_of_error ** 3) * (0.0001 * abs(innovation)) + (0.02 * abs(self.prev_x - self.x))
         self.uncertainty = self.uncertainty + Q
         if (self.uncertainty > 1):
             self.uncertainty = 1
@@ -66,6 +66,8 @@ class KalmanFilter1D:
         # K = self.uncertainty / (self.uncertainty + variance_measure)
         # Custom Kalman
         K = self.uncertainty
+        if (K < 0.1):
+            K = 0.1
         print(K)
         self.x = self.x + K * innovation
         #4 Sample rule (it should take about 4 samples from a spike to reach mean)
@@ -74,22 +76,50 @@ class KalmanFilter1D:
 
         return self.x
 
+n = 1
+prev_average = 0
+average = 0
+
+prev_actual = 0
+
+def resetMeanMeasure():
+    global n
+    n = 1
+    global prev_average
+    prev_average = limelight.reading
+    global average 
+    average = limelight.reading
 
 # Simulation
 limelight = Limelight()
 kf = KalmanFilter1D()
 
-for i in range(350):
+
+for i in range(200):
     limelight.update()
+    if prev_actual != limelight.actual:
+        resetMeanMeasure()
+    prev_actual = limelight.actual
     estimate = kf.update(limelight.reading, limelight.sdEstimate)
     plt.scatter(i,limelight.reading,color = "red")
     plt.scatter(i,estimate,color = "blue")
+    plt.scatter(i,limelight.actual,color = "green")
     print(f"Measured: {limelight.reading:.2f}, Estimate: {estimate:.2f}, Actual: {limelight.actual}")
     #limelight.setActual(10 + (i/2))
+
     if i == 50:
         limelight.setActual(140)
     if i == 100:
         limelight.setActual(90)
     if i == 150:
-          limelight.setActual(80)  
+          limelight.setActual(80) 
+    if i == 200:
+          limelight.setActual(50)
+    if i > 200:
+        limelight.setActual(i - 150)
+    average = prev_average + (limelight.reading - prev_average)/n
+    n = n + 1
+    prev_average = average
+    plt.scatter(i,average,color = "black")
+    
 plt.show()
