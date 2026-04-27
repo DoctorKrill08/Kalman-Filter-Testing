364b7ba60c30aaf08a5b233447b9588622b6f6da
DoctorKrill08 <doctorkrill08@gmail.com>
2026-04-26 23:34:32 -0400

    P based Kalman

--- Kalman.py
+++ Kalman.py
@@ -7,7 +7,7 @@ import random
 class Limelight:
     @staticmethod
     def pose_to_sd(pose):
-        return pose / 5;
+        return pose / 10;
     def __init__(self):
         self.actual = 50
         self.sd = self.pose_to_sd(self.actual)
@@ -19,39 +19,56 @@ class Limelight:
         self.sd = self.pose_to_sd(actual)
 
     def update(self):
-        self.reading = self.sd * 2 * (0.5 - random.random()) + self.actual
+        self.reading = random.normalvariate(self.actual,self.sd)
         self.sdEstimate = self.pose_to_sd(self.reading)
 
 
 class KalmanFilter1D:
-    def __init__(self, initial_estimate=1, initial_uncertainty=1000):
+    def __init__(self, initial_estimate=1, initial_uncertainty=1):
         self.x = initial_estimate
-        self.P = initial_uncertainty
+        self.uncertainty = initial_uncertainty
+        self.sd = 1
+        self.prev_measurement = 0
+        self.prev_sd = 1
 
+    def two_step_verification(self,measurement, measurement_sd):
+        result = False
+        if (measurement > self.prev_measurement):
+            result = self.prev_measurement > measurement - (1.5 * measurement_sd)
+        else:
+            result = measurement > self.prev_measurement - (1.5 * self.prev_sd)
+        self.prev_sd = measurement_sd
+        self.prev_measurement = measurement
+        return result
     def update(self, measurement, measurement_sd):
-        R = measurement_sd ** 2
+        variance_measure = measurement_sd ** 2
+        #CUSTOMIZED normalize variance
+        self.sd = Limelight.pose_to_sd(self.x)
+        variance = self.sd ** 2
+        if (not self.two_step_verification(measurement,measurement_sd)):
+            return self.x
+        delta_sd = abs(self.sd - measurement_sd)
+        
 
         innovation = measurement - self.x
-        S = self.P + R
 
-        # --- TELEPORT / OUTLIER DETECTION ---
-        threshold = 1.5  # ~99.7% confidence (3-sigma rule)
-
-        sdError = abs(Limelight.pose_to_sd(self.x) - measurement_sd)
-        Q = (sdError / Limelight.pose_to_sd(self.x)) + abs(innovation)/(threshold * math.sqrt(abs(S)))
-
-        if abs(innovation) > threshold * math.sqrt(abs(S)) or (Q > threshold):
-            # Option A: hard reset (fastest recovery)
-            self.x = measurement
-            self.P = R
-            return self.x
-
-        self.P = self.P + Q
+        z_score_of_error = abs(innovation) / measurement_sd
+        # < 2 z-score -> nada. difference from 2
+        Q = (z_score_of_error ** 2) * (0.0005 * abs(innovation))
+        self.uncertainty = self.uncertainty + Q
+        if (self.uncertainty > 1):
+            self.uncertainty = 1
+        print(Q)
 
         # --- NORMAL KALMAN UPDATE ---
-        K = self.P / (self.P + R)
+        # K = self.uncertainty / (self.uncertainty + variance_measure)
+        # Custom Kalman
+        K = self.uncertainty
+        print(K)
         self.x = self.x + K * innovation
-        self.P = (1 - K) * self.P
+        #4 Sample rule (it should take about 4 samples from a spike to reach mean)
+        #Therefore deterioation should be
+        self.uncertainty = self.uncertainty / 1.4
 
         return self.x
 
@@ -66,12 +83,9 @@ for i in range(250):
     plt.scatter(i,limelight.reading,color = "red")
     plt.scatter(i,estimate,color = "blue")
     print(f"Measured: {limelight.reading:.2f}, Estimate: {estimate:.2f}, Actual: {limelight.actual}")
+    #limelight.setActual(10 + (i/2))
     if i == 50:
-        limelight.setActual(60)
-    if i == 100:
         limelight.setActual(140)
-    if i == 150:
-        limelight.setActual(120)
-    if i == 200:
-        limelight.setActual(20)
+    if i == 100:
+        limelight.setActual(90)
 plt.show()
